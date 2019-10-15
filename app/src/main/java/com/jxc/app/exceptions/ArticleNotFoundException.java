65e2de46d7ff5ce2af6896267ec98c38e82f92ca
nrc97 <nog5fmox>
2019-10-15 05:33:57 +0200

    feature article: updated User model, services and controller, and added Article model and ArticleConsultationDTO model

---
+++ app/src/main/java/com/jxc/app/exceptions/ArticleNotFoundException.java
@@ -0,0 +1,11 @@
+package com.jxc.app.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ArticleNotFoundException extends RuntimeException {
+    public ArticleNotFoundException(String message) {
+        super(message);
+    }
+}
