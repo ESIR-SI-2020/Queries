178e4c6bdf2a09c0df69ef417a63dd4ff4519de1
Kévin PHILIP <kevin.philip@outlook.fr>
2019-10-21 13:47:35 +0200

    feature(init-graphql)

---
+++ src/main/java/com/jxc/readapis/graphql/resolvers/QueryResolver.java
@@ -0,0 +1,19 @@
+package com.jxc.readapis.graphql.resolvers;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.jxc.dbmanager.models.User;
+import com.jxc.dbmanager.repositories.UserRepository;
+import com.jxc.readapis.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QueryResolver implements GraphQLQueryResolver {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User userByEmail(String email){
+        return userRepository.findById(email).orElseThrow(() -> new UserNotFoundException("User with email : "+ email + " does not exist."));
+    }
+}
---
+++ src/main/java/com/jxc/readapis/graphql/resolvers/UserResolver.java
@@ -0,0 +1,32 @@
+package com.jxc.readapis.graphql.resolvers;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.coxautodev.graphql.tools.GraphQLResolver;
+import com.jxc.dbmanager.models.Address;
+import com.jxc.dbmanager.models.Article;
+import com.jxc.dbmanager.models.User;
+import com.jxc.dbmanager.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class UserResolver implements GraphQLResolver<User> {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public Address address(User user){
+        return user.getAddress();
+    }
+
+    public List<Article> articles(User user){
+        return user.getArticles();
+    }
+
+    public List<Article> sharedArticles(User user){
+        return user.getSharedArticles();
+    }
+
+}
