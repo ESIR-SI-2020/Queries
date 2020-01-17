2e90e59c49d216b11ddfa458243d5f407dc923bd
salomejarnouen <salomejarnouen@gmail.com>
2020-01-17 12:55:43 +0100

    feature(AnalyticsUserCreatedToday) : add findOnyById and delete methods

--- src/main/java/com/jxc/readapis/controllers/UserAnalyticsController.java
+++ src/main/java/com/jxc/readapis/controllers/UserAnalyticsController.java
@@ -3,14 +3,11 @@ package com.jxc.readapis.controllers;
 import com.jxc.readapis.dto.Count;
 import com.jxc.readapis.services.UserAnalyticsService;
 import fr.esir.jxc.domain.models.analytics.UserAdded;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.ZoneId;
@@ -41,9 +38,25 @@ public class UserAnalyticsController {
     @GetMapping("/nbofuserscreatedtoday")
     public ResponseEntity<Count> getNbOfUsersCreatedToday() {
         LocalDate localDate = LocalDate.now();
-        Date today = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
-        List<UserAdded> userAddedTodayList = userAnalyticsService.findAllUserAddedByDate(today);
+
+        Date dateDebut = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date dateFin = Date.from(localDate.atTime(23, 59, 59).atZone(ZoneId.systemDefault()).toInstant());
+
+        List<UserAdded> userAddedTodayList = userAnalyticsService.findAllUserAddedByDateInterval(dateDebut, dateFin);
 
         return new ResponseEntity<>(new Count(userAddedTodayList.size()), HttpStatus.OK);
+    }
+
+
+    @DeleteMapping("/deleteuseradded/{id}")
+    public ResponseEntity<HttpStatus> deleteUserAdded(@PathVariable String id) {
+        UserAdded userAdded = userAnalyticsService.findOneById(id);
+
+        String documentId = userAnalyticsService.delete(userAdded);
+        if(documentId != null) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/jxc/readapis/services/UserAnalyticsServiceImpl.java
+++ src/main/java/com/jxc/readapis/services/UserAnalyticsServiceImpl.java
@@ -1,12 +1,18 @@
 package com.jxc.readapis.services;
 
 import fr.esir.jxc.domain.models.analytics.UserAdded;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
+import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
 import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -16,6 +22,31 @@ public class UserAnalyticsServiceImpl implements UserAnalyticsService {
     @Autowired
     private ElasticsearchOperations elasticsearchTemplate;
 
+    @Override
+    public UserAdded save(UserAdded userAdded) {
+        Assert.notNull(userAdded, "Cannot save null entity.");
+
+        this.elasticsearchTemplate.putMapping(UserAdded.class);
+        this.elasticsearchTemplate.index(new IndexQueryBuilder().withObject(userAdded).build());
+        this.elasticsearchTemplate.refresh(UserAdded.class);
+
+        return userAdded;
+    }
+
+    public String delete(UserAdded userAdded) {
+        return this.elasticsearchTemplate.delete(UserAdded.class, userAdded.getId());
+    }
+
+
+    public UserAdded findOneById(String id) {
+        Collection<String> ids = new ArrayList<String>();
+        ids.add(id);
+        SearchQuery searchQuery = new NativeSearchQueryBuilder().withIds(ids).build();
+        List<UserAdded> result = this.elasticsearchTemplate.queryForList(searchQuery, UserAdded.class);
+
+        return result.get(0);
+    }
+
     public List<UserAdded> findAllUserAdded() {
         SearchQuery searchQuery = new NativeSearchQueryBuilder().build();
         return this.elasticsearchTemplate.queryForList(searchQuery, UserAdded.class);
@@ -23,8 +54,18 @@ public class UserAnalyticsServiceImpl implements UserAnalyticsService {
 
 
     @Override
-    public List<UserAdded> findAllUserAddedByDate (Date date) {
+    public List<UserAdded> findAllUserAddedByDateInterval(Date dateDebut, Date dateFin) {
+       /*SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                                        .withQuery(matchQuery("creationDate", dateDebut))
+                                        .build();*/
+      //  SearchSourceBuilder builder = new SearchSourceBuilder();
+       // builder.query(QueryBuilders.rangeQuery("creationDate").from(dateDebut).to(dateFin));
+        String dateDebutFormat = DateFormat.getDateInstance().format(dateDebut);
+        String dateFinFormat = DateFormat.getDateInstance().format(dateDebut);
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(QueryBuilders.rangeQuery("creationDate").gte(dateDebutFormat).lte(dateFinFormat))
+                .build();
 
-        return null;
+        return this.elasticsearchTemplate.queryForList(searchQuery, UserAdded.class);
     }
 }
