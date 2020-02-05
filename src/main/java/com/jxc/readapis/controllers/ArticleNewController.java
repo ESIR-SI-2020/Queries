3063da65d0e93838162c756e4739d02d887e0382
oumar <balloumar49@hotmail.com>
2020-02-06 00:06:24 +0100

    wip

--- src/main/java/com/jxc/readapis/controllers/ArticleNewController.java
+++ src/main/java/com/jxc/readapis/controllers/ArticleNewController.java
@@ -10,6 +10,8 @@ import java.util.concurrent.ExecutionException;
 
 import fr.esir.jxc.domain.models.analytics.UserAdded;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.core.*;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
@@ -21,6 +23,8 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.query.NativeSearchQueryBuilder;
+import org.springframework.data.elasticsearch.core.query.SearchQuery;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,19 +37,61 @@ import org.springframework.web.bind.annotation.RestController;
 public class ArticleNewController {
 
     @Autowired
+    private  ElasticsearchTemplate elasticsearchTemplate;
+
+    @Autowired
     Client client;
-    @PostMapping("/create")
+    @PostMapping("/create_useradded")
     public String create(@RequestBody UserAdded userAdded) throws IOException {
 
         IndexResponse response = client.prepareIndex("pocket", "user", userAdded.getId())
                 .setSource(jsonBuilder()
                         .startObject()
-                        .field("name", userAdded.getCreationDate())
+                        .field("creationDate", userAdded.getCreationDate())
                         .endObject()
                 )
                 .get();
         System.out.println("response id:"+response.getId());
         return response.getResult().toString();
+    }
+
+    @GetMapping("/useraddedbyid/{id}")
+    public Map<String, Object> view(@PathVariable final String id) {
+        GetResponse getResponse = client.prepareGet("pocket", "user", id).get();
+        return getResponse.getSource();
+    }
+
+    @GetMapping("/useradded/creationDate/{field}")
+    public Map<String, Object> searchByName(@PathVariable final String field) {
+        Map<String,Object> map = null;
+        SearchResponse response = client.prepareSearch("pocket")
+                .setTypes("user")
+                .setSearchType(SearchType.QUERY_AND_FETCH)
+                .setQuery(QueryBuilders.matchQuery("creationDate", field))
+                .get()
+                ;
+        List<SearchHit> searchHits = Arrays.asList(response.getHits().getHits());
+        map =   searchHits.get(0).getSource();
+        return map;
+    }
+
+  /*  @GetMapping("/all")
+    public List<UserAdded> getUsersAdded(String apartmentName) {
+        SearchQuery searchQuery = new NativeSearchQueryBuilder()
+                .withQuery(org.elasticsearch.index.query.QueryBuilders
+                        .matchQuery("apartment_Name", apartmentName)).build();
+        Page<UserAdded> sampleEntities =
+                elasticsearchTemplate.queryForPage(searchQuery,UserAdded.class);
+        return sampleEntities.getContent();
+    }*/
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable final String id) {
+
+        DeleteResponse deleteResponse = client.prepareDelete("pocket", "user", id).get();
+
+        System.out.println(deleteResponse.getResult().toString());
+        return deleteResponse.getResult().toString();
     }
 
 }
